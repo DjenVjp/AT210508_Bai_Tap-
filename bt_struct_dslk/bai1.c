f601efd537c0ff4fab20d30879b71d8d8b868cd0
DjenVjp <dienvip242@gmail.com>
2025-04-14 16:14:57 +0700

    Add file

--- bt_struct_dslk/bai1.c
+++ bt_struct_dslk/bai1.c
@@ -2,26 +2,33 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-int tienhang (int a, int b){
-    return a*b;
-}
-struct banhang {
+struct banhang
+{
     char ten[100];
-    int gia;
+    double gia;
     int sl;
+    double th;
 };
 typedef struct banhang banhang;
-void nhap(banhang a ){
-    printf("nhap ten hang : "); gets(a.ten);
-    printf("nhap don gia : "); scanf("%d", &a.gia);
-    printf("nhap so luong hang : "); scanf("%d", &a.sl);
+void nhap(banhang *a)
+{
+    printf("nhap ten hang : ");
+    gets((*a).ten);
+    printf("nhap don gia : ");
+    scanf("%lf", &(*a).gia);
+    printf("nhap so luong hang : ");
+    scanf("%d", &(*a).sl);
     getchar();
 }
-void sapxep(banhang a[], int n){
-    for(int i = 0; i < n; i++){
+void sapxep(banhang a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         int pos;
-        for(int j = i + 1; j < n; j++){
-            if(strcmp(a[i].ten, a[j].ten) == 1){
+        for (int j = i + 1; j < n; j++)
+        {
+            if (strcmp(a[i].ten, a[j].ten) == 1)
+            {
                 pos = j;
                 banhang b = a[i];
                 a[i] = a[pos];
@@ -30,17 +37,35 @@ void sapxep(banhang a[], int n){
         }
     }
 }
-void in(banhang a){
-    printf("%20s %20d %")
-}
-int main(){
+int main()
+{
     int n;
     printf("so luong hang cua ban la : ");
     scanf("%d", &n);
     getchar();
     banhang a[n];
-    for(int i = 0; i < n; i++){
-        nhap(a[i]);
+    for (int i = 0; i < n; i++)
+    {
+        nhap(&a[i]);
+        a[i].th = a[i].gia* a[i].sl;
     }
-    sapxep(a);
+    sapxep(a, n);
+    double tong = 0;
+    for (int i = 0; i < n; i++)
+    {
+        tong += a[i].th;
+    }
+    printf("STT");
+    printf("            Ten hang");
+    printf("              Don gia");
+    printf("             So luong");
+    printf("           Thanh tien\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%3d", i + 1);
+        printf("%20s %20.2lf %20d %20.2lf", a[i].ten, a[i].gia, a[i].sl, a[i].th);
+        printf("\n");
+    }
+    printf("                                                        Tong tien:%20.2lf", tong);
+    return 0;
 }
